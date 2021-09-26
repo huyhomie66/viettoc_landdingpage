4904cde93249c7a9e32d0af8e230ba7e6da9dbcd
Trần Huy <39986576+huyhomie66@users.noreply.github.com>
2021-09-26 19:02:21 +0700

    header

---
+++ src/components/Body.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./body.css";
+const Banner = () => {
+  return <div className="banner"></div>;
+};
+
+const Body = () => {
+  return (
+    <div class="content">
+      <Banner />
+    </div>
+  );
+};
+
+export default Body;
---
+++ src/components/Header.js
@@ -0,0 +1,78 @@
+import "./navbar-mobile.css";
+import "./navbar-desktop.css";
+import "./header.css";
+import logo from "../assets/logo.png";
+
+function NavBarMobile({
+  navbarItems = [],
+  register = "",
+  onRegister = () => {},
+}) {
+  return (
+    <div className="navbar-mobile">
+      <input id="toggle" type="checkbox" />
+
+      <label className="toggle-container" for="toggle">
+        <span className="button button-toggle"></span>
+      </label>
+
+      <nav className="nav">
+        {navbarItems.map((e) => {
+          return (
+            <a className="nav-item" href="/">
+              {e.title}
+            </a>
+          );
+        })}
+      </nav>
+    </div>
+  );
+}
+
+function NavbarDesktop({
+  navbarItems = [],
+  register = "",
+  onRegister = () => {},
+}) {
+  return (
+    <div className="navbar-desktop">
+      <img alt="logo" src={logo} />
+      <div className="titles">
+        {navbarItems.map((e) => {
+          return <a href="/">{e.title}</a>;
+        })}
+      </div>
+
+      <button onClick={onRegister} style={{ backgroundColor: "#9E1C1E" }}>
+        {register}
+      </button>
+    </div>
+  );
+}
+
+function Header() {
+  const navbarItems = [
+    { title: "Trang chủ" },
+    { title: "Chính sách" },
+    { title: "Bảng giá" },
+    { title: "Bài viết" },
+    { title: "Liên hệ" },
+  ];
+
+  const register = "Đăng ký cho dòng họ";
+
+  const props = {
+    navbarItems,
+    register,
+    onRegister: () => {},
+  };
+
+  return (
+    <header>
+      <NavbarDesktop {...props} />
+      <NavBarMobile {...props} />
+    </header>
+  );
+}
+
+export default Header;
