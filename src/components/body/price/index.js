0466f10483b0944b321a1bdff8637cb6df667042
Trần Huy <39986576+huyhomie66@users.noreply.github.com>
2021-09-30 08:10:47 +0700

    register

--- src/components/body/price/index.js
+++ src/components/body/price/index.js
@@ -4,6 +4,165 @@ import arrowRight from "../../../assets/arrow-right.png";
 import rectangle from "../../../assets/rectangle.png";
 import { Fragment } from "react";
 
+const title = "CÁC GÓI TẠI VIỆT TỘC";
+const price = {
+  normal: [
+    {
+      price: { value: "0" },
+      title: {
+        value: "Khởi đầu",
+        className: "price-normal-title",
+      },
+      description: [
+        {
+          bold: "320 thành viên",
+          normal: "trong cây gia phả",
+        },
+        {
+          bold: "25 GB",
+          normal: "dung lượng lưu trữ",
+        },
+      ],
+      button: {
+        title: "Xem thêm",
+        onClick: () => {},
+        className: "price-normal-button",
+      },
+    },
+    {
+      price: { value: "4.000.000" },
+      title: {
+        value: "Cơ bản",
+        className: "price-normal-title",
+      },
+      description: [
+        {
+          bold: "200 thành viên",
+          normal: "trong cây gia phả",
+        },
+        {
+          bold: "20 GB",
+          normal: "dung lượng lưu trữ",
+        },
+      ],
+      button: {
+        title: "Xem thêm",
+        onClick: () => {},
+        className: "price-normal-button",
+      },
+    },
+    {
+      promotion: { value: "Phổ biến nhất" },
+
+      price: { value: "5.000.000" },
+      title: {
+        value: "Đoàn viên",
+        className: "price-normal-title",
+      },
+      description: [
+        {
+          bold: "320 thành viên",
+          normal: "trong cây gia phả",
+        },
+        {
+          bold: "25 GB",
+          normal: "dung lượng lưu trữ",
+        },
+      ],
+      button: {
+        title: "Xem thêm",
+        className: "price-normal-button",
+        onclick: () => {},
+      },
+    },
+  ],
+  special: [
+    {
+      className: "special-1",
+      price: { value: "15.000.000" },
+      title: {
+        value: "ĐỒNG TÂM",
+      },
+      description: [
+        {
+          bold: "1000 thành viên",
+          normal: "trong cây gia phả",
+        },
+        {
+          bold: "100 GB",
+          normal: "dung lượng lưu trữ",
+        },
+      ],
+      button: {
+        title: "Xem thêm",
+        className: "price-special-title",
+        onClick: () => {},
+      },
+    },
+    {
+      className: "special-2",
+      promotion: {
+        value: "Phổ biến nhất",
+        className: "price-special-promotion",
+      },
+      price: {
+        value: "50.000.000",
+        className: "text-gold",
+        unitClassName: "text-white",
+      },
+      title: {
+        value: "THỊNH VƯỢNG",
+        className: "text-gold",
+      },
+      description: [
+        {
+          bold: "1000 thành viên",
+          normal: "trong cây gia phả",
+          className: "text-white",
+        },
+        {
+          bold: "100 GB",
+          normal: "dung lượng lưu trữ",
+          className: "text-white",
+        },
+      ],
+      button: {
+        title: "Xem thêm",
+        className: "price-special-title",
+        onClick: () => {},
+      },
+    },
+    {
+      className: "special-3",
+      promotion: {
+        value: "Đặc biệt",
+        className: "price-special-promotion",
+      },
+      title: {
+        value: "BẢN SẮC",
+        className: "price-title-special",
+      },
+      description: [
+        {
+          bold: "1000 thành viên",
+          normal: "trong cây gia phả",
+          className: "text-white",
+        },
+        {
+          bold: "100 GB",
+          normal: "dung lượng lưu trữ",
+          className: "text-white",
+        },
+      ],
+      button: {
+        title: "Liên hệ",
+        className: "price-special-button",
+        onClick: () => {},
+      },
+    },
+  ],
+};
+
 const PriceBanner = ({
   className = "",
   price,
@@ -66,192 +225,32 @@ const PriceBanner = ({
   );
 };
 
-const Price = () => {
-  const price = {
-    normal: [
-      {
-        price: { value: "0" },
-        title: {
-          value: "Khởi đầu",
-          className: "price-normal-title",
-        },
-        description: [
-          {
-            bold: "320 thành viên",
-            normal: "trong cây gia phả",
-          },
-          {
-            bold: "25 GB",
-            normal: "dung lượng lưu trữ",
-          },
-        ],
-        button: {
-          title: "Xem thêm",
-          onClick: () => {},
-          className: "price-normal-button",
-        },
-      },
-      {
-        price: { value: "4.000.000" },
-        title: {
-          value: "Cơ bản",
-          className: "price-normal-title",
-        },
-        description: [
-          {
-            bold: "200 thành viên",
-            normal: "trong cây gia phả",
-          },
-          {
-            bold: "20 GB",
-            normal: "dung lượng lưu trữ",
-          },
-        ],
-        button: {
-          title: "Xem thêm",
-          onClick: () => {},
-          className: "price-normal-button",
-        },
-      },
-      {
-        promotion: { value: "Phổ biến nhất" },
+const Title = () => (
+  <div className="price-container-title row item-center">
+    <img alt="arrow-left" src={arrowLeft} />
+    <h1>{title}</h1>
+    <img alt="arrow-right" src={arrowRight} />
+  </div>
+);
 
-        price: { value: "5.000.000" },
-        title: {
-          value: "Đoàn viên",
-          className: "price-normal-title",
-        },
-        description: [
-          {
-            bold: "320 thành viên",
-            normal: "trong cây gia phả",
-          },
-          {
-            bold: "25 GB",
-            normal: "dung lượng lưu trữ",
-          },
-        ],
-        button: {
-          title: "Xem thêm",
-          className: "price-normal-button",
-          onclick: () => {},
-        },
-      },
-    ],
-    special: [
-      {
-        className: "special-1",
-        price: { value: "15.000.000" },
-        title: {
-          value: "ĐỒNG TÂM",
-        },
-        description: [
-          {
-            bold: "1000 thành viên",
-            normal: "trong cây gia phả",
-          },
-          {
-            bold: "100 GB",
-            normal: "dung lượng lưu trữ",
-          },
-        ],
-        button: {
-          title: "Xem thêm",
-          className: "price-special-title",
-          onClick: () => {},
-        },
-      },
-      {
-        className: "special-2",
-        promotion: {
-          value: "Phổ biến nhất",
-          className: "price-special-promotion",
-        },
-        price: {
-          value: "50.000.000",
-          className: "text-gold",
-          unitClassName: "text-white",
-        },
-        title: {
-          value: "THỊNH VƯỢNG",
-          className: "text-gold",
-        },
-        description: [
-          {
-            bold: "1000 thành viên",
-            normal: "trong cây gia phả",
-            className: "text-white",
-          },
-          {
-            bold: "100 GB",
-            normal: "dung lượng lưu trữ",
-            className: "text-white",
-          },
-        ],
-        button: {
-          title: "Xem thêm",
-          className: "price-special-title",
-          onClick: () => {},
-        },
-      },
-      {
-        className: "special-3",
-        promotion: {
-          value: "Đặc biệt",
-          className: "price-special-promotion",
-        },
-        title: {
-          value: "BẢN SẮC",
-          className: "price-title-special",
-        },
-        description: [
-          {
-            bold: "1000 thành viên",
-            normal: "trong cây gia phả",
-            className: "text-white",
-          },
-          {
-            bold: "100 GB",
-            normal: "dung lượng lưu trữ",
-            className: "text-white",
-          },
-        ],
-        button: {
-          title: "Liên hệ",
-          className: "price-special-button",
-          onClick: () => {},
-        },
-      },
-    ],
-  };
-
-  const Title = () => (
-    <div className="price-container-title row item-center">
-      <img alt="arrow-left" src={arrowLeft} />
-      <h1>{title}</h1>
-      <img alt="arrow-right" src={arrowRight} />
+const Promotions = () => (
+  <Fragment>
+    <div className="price-normal row">
+      {price.normal.map((props, key) => (
+        <PriceBanner {...props} key={key} />
+      ))}
     </div>
-  );
-
-  const Promotions = () => (
-    <Fragment>
-      <div className="price-normal row">
-        {price.normal.map((props, key) => (
+    <div className="price-special">
+      <div className="row">
+        {price.special.map((props, key) => (
           <PriceBanner {...props} key={key} />
         ))}
       </div>
-      <div className="price-special">
-        <div className="row">
-          {price.special.map((props, key) => (
-            <PriceBanner {...props} key={key} />
-          ))}
-        </div>
-      </div>
-    </Fragment>
-  );
+    </div>
+  </Fragment>
+);
 
-  const title = "CÁC GÓI TẠI VIỆT TỘC";
-
+const Price = () => {
   return (
     <div className="price-container  col item-center">
       <Title />
--- src/components/body/register/index.js
+++ src/components/body/register/index.js
@@ -1,7 +1,64 @@
+import { useState } from "react";
 import "./styles.css";
+import back from "../../../assets/back.png";
+
+const Modal = ({ children }) => {
+  return <div className="modal col  ">{children}</div>;
+};
+
+const steps = [
+  {
+    title: "Khởi tạo tài khoản dòng họ",
+    description:
+      "Để đưa dòng họ của mình lên Việt Tộc, vui lòng nhập các thông tin sau để tạo một tài khoản quản lý",
+    fields: [
+      { name: "family_name", label: "Tên dòng họ *" },
+      { name: "family_level", label: "Dòng họ của bạn có bao nhiêu đinh *" },
+    ],
+    next: "Tiếp theo",
+  },
+  {
+    title: "Khởi tạo tài khoản dòng họ",
+
+    fields: [
+      { name: "provider", label: "Tỉnh" },
+      { name: "district", label: "Huyện" },
+      { name: "commune", label: "Xã" },
+      { name: "address_detail", label: "Địa chỉ chi tiết" },
+    ],
+    next: "Tiếp theo",
+  },
+  {
+    title: "Thông Tin Người Quản Lý",
+    fields: [
+      { name: "full_name", label: "Họ và tên" },
+      { name: "phone", label: "Số điện thoại" },
+      { name: "email", label: "Email" },
+    ],
+    next: "Hoàn tất đăng ký",
+  },
+  {
+    title: "Đăng ký thành công",
+    description:
+      "Cho phép người dùng thay đổi số điện thoại và email để đăng ký tài khoản khác.",
+
+    next: "Trở về trang chủ",
+  },
+];
 
 const Register = () => {
-  return <div className="register-banner"></div>;
+  const [currentStep, setStep] = useState(0);
+  return (
+    <div className="register-banner col item-center content-center ">
+      <Modal>
+        <div className="row modal-head item-center">
+          <img src={back} alt="back" /> <h4>Bước {currentStep}/3</h4>
+        </div>
+
+        <button>{steps[currentStep].next}</button>
+      </Modal>
+    </div>
+  );
 };
 
 export default Register;
