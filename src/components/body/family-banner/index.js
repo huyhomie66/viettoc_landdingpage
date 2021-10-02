8cec221e97c4fab354fcd9c24d0f64eda83395ce
unknown <phamhande@gmail.com>
2021-10-02 16:59:43 +0700

    family page

--- src/components/body/family-banner/index.js
+++ src/components/body/family-banner/index.js
@@ -1,18 +1,26 @@
 import "./styles.css";
-
+import tickit from '../../../assets/tickitFamily.png'
 const FamilyBanner = () => {
   return (
     <div className="family-banner">
       <div className="family-info col content-around">
-        <h1>Việt Tộc giúp dòng họ chúng tôi gắn kết hơn</h1>
-        <p>
+        <div className='family-container'>
+          <div style={{display:'flex',alignSelf:'center'}}>
+          <img className='family-tickit' src={tickit}/>
+          <div>
+          <h1 className='family-title'>Việt Tộc giúp dòng họ chúng tôi gắn kết hơn</h1>
+          <p className='family-content'>
           Tôi rất vui vì có một ứng dụng dành riêng cho dòng họ. Việt Tộc đã và
           đang giúp ích chúng tôi rất nhiều trong việc kết nối anh em họ hàng,
           để mọi người có một không gian chung, đóng góp cho sự phát triển
           trường vinh của chi họ.
         </p>
-        <h3>Bác Lê Trường Sơn</h3>
-        <p>Chi họ Lê - Thuận Thành, Bắc Ninh</p>
+        <h3 className='family-arthor'>Bác Lê Trường Sơn</h3>
+        <p className='family-address'>Chi họ Lê - Thuận Thành, Bắc Ninh</p>
+          </div>
+          </div>
+       
+        </div>
       </div>
     </div>
   );
