40bab3e8a39336b75689fd08b01afe81c3f451a2
Trần Huy <39986576+huyhomie66@users.noreply.github.com>
2021-10-05 08:09:33 +0700

    responesive

--- src/components/body/intro/Block.js
+++ src/components/body/intro/Block.js
@@ -3,25 +3,43 @@ import intro from "../../../assets/intro.png";
 import flower from "../../../assets/flower.png";
 import React from "react";
 
-const Block = ({ description, buttonTitle }) => {
-    return (
-        <div className="block-container">
-            <div className="flower">
-                <img alt="flower" src={flower} />
-            </div>
-            <div className="block-body row">
-                <div className="title col item-center content-center">
-                    <img src={intro} alt="intro" />
-                </div>
-                <div className="col">
-                    <p>{description}</p>
-                    <button>{buttonTitle}</button>
-                </div>
-            </div>
-            <div className="block-footer" />
+const BlockDesktop = ({ description, buttonTitle }) => {
+  return (
+    <React.Fragment>
+      <div className="flower">
+        <img alt="flower" src={flower} />
+      </div>
+      <div className="block-body row">
+        <div className="title col item-center content-center">
+          <img src={intro} alt="intro" />
         </div>
-    );
+        <div className="col">
+          <p>{description}</p>
+          <button>{buttonTitle}</button>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
+const BlockMobile = () => {
+  return (
+    <React.Fragment>
+      <div className="block-mobile col">
+        <div className="bg-blue "></div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+const Block = ({ description, buttonTitle }) => {
+  return (
+    <div className="block-container">
+      {/* <BlockDesktop description={description} buttonTitle={buttonTitle} /> */}
+      <BlockMobile />
+      <div className="block-footer" />
+    </div>
+  );
+};
 
 export default Block;
